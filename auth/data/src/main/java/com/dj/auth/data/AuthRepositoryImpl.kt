d7d918b865b8436e35fc2f71b181f6061f8b7393
DJDrama <putzzang@naver.com>
2024-05-06 02:15:47 +0900

    27. Logging in users

--- auth/data/src/main/java/com/dj/auth/data/AuthRepositoryImpl.kt
+++ auth/data/src/main/java/com/dj/auth/data/AuthRepositoryImpl.kt
@@ -2,14 +2,34 @@ package com.dj.auth.data
 
 import com.dj.auth.domain.AuthRepository
 import com.dj.core.data.networking.post
+import com.dj.core.domain.AuthInfo
+import com.dj.core.domain.SessionStorage
 import com.dj.core.domain.util.DataError
 import com.dj.core.domain.util.EmptyResult
+import com.dj.core.domain.util.Result
+import com.dj.core.domain.util.asEmptyDataResult
 import io.ktor.client.HttpClient
 
-class AuthRepositoryImpl
-constructor(
-    private val httpClient: HttpClient
+class AuthRepositoryImpl(
+    private val httpClient: HttpClient,
+    private val sessionStorage: SessionStorage
 ) : AuthRepository {
+    override suspend fun login(email: String, password: String): EmptyResult<DataError.Network> {
+        val result = httpClient.post<LoginRequest, LoginResponse>(
+            route = "/login",
+            body = LoginRequest(email = email, password = password)
+        )
+       if(result is Result.Success)
+            sessionStorage.set(
+                AuthInfo(
+                    accessToken = result.data.accessToken,
+                    refreshToken = result.data.refreshToken,
+                    userId = result.data.userId
+                )
+            )
+        return result.asEmptyDataResult()
+    }
+
     override suspend fun register(email: String, password: String): EmptyResult<DataError.Network> {
         return httpClient.post<RegisterRequest, Unit>(
             route = "/register",
--- auth/domain/src/main/java/com/dj/auth/domain/AuthRepository.kt
+++ auth/domain/src/main/java/com/dj/auth/domain/AuthRepository.kt
@@ -4,5 +4,6 @@ import com.dj.core.domain.util.DataError
 import com.dj.core.domain.util.EmptyResult
 
 interface AuthRepository {
+    suspend fun login(email: String, password: String): EmptyResult<DataError.Network>
     suspend fun register(email: String, password: String): EmptyResult<DataError.Network>
 }
