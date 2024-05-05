797c7f6e01119c5078aaa45cc534d6f0b7f31303
DJDrama <putzzang@naver.com>
2024-05-06 00:59:26 +0900

    24. Setting up session storage

--- core/data/src/main/java/com/dj/core/data/di/CoreDataModule.kt
+++ core/data/src/main/java/com/dj/core/data/di/CoreDataModule.kt
@@ -1,10 +1,16 @@
 package com.dj.core.data.di
 
+import android.content.SharedPreferences
+import com.dj.core.data.auth.EncryptedSessionStorage
 import com.dj.core.data.networking.HttpClientFactory
+import com.dj.core.domain.SessionStorage
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val coreDataModule = module {
     single {
         HttpClientFactory().build()
     }
+    singleOf(::EncryptedSessionStorage).bind<SessionStorage>()
 }
