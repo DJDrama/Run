cfaa620ae0d7cb9bb7d6dad32680c895be02d9f6
DJDrama <putzzang@naver.com>
2024-05-05 20:04:59 +0900

    19. Intro Screen UI

---
+++ auth/presentation/src/main/java/com/dj/auth/presentation/intro/IntroScreen.kt
@@ -0,0 +1,121 @@
+package com.dj.auth.presentation.intro
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.dj.auth.presentation.R
+import com.dj.core.presentation.designsystem.LogoIcon
+import com.dj.core.presentation.designsystem.RunTheme
+import com.dj.core.presentation.designsystem.components.GradientBackground
+import com.dj.core.presentation.designsystem.components.RunActionButton
+import com.dj.core.presentation.designsystem.components.RunOutlinedActionButton
+
+@Composable
+fun IntroScreenRoot(
+    onSignUpClick: () -> Unit,
+    onSignInClick: () -> Unit,
+) {
+    IntroScreen { action ->
+        when (action) {
+            IntroAction.OnSignInClick -> onSignInClick()
+            IntroAction.OnSignUpClick -> onSignUpClick
+        }
+    }
+}
+
+@Composable
+fun IntroScreen(
+    onAction: (IntroAction) -> Unit
+) {
+    GradientBackground {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            RunLogoVertical()
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .padding(bottom = 48.dp)
+        ) {
+
+            Text(
+                text = stringResource(id = R.string.welcome_to_runrun),
+                color = MaterialTheme.colorScheme.onBackground,
+                fontSize = 20.sp
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = stringResource(id = R.string.runrun_description),
+                style = MaterialTheme.typography.bodySmall
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+            RunOutlinedActionButton(
+                text = stringResource(id = R.string.sign_in),
+                isLoading = false,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                onAction(IntroAction.OnSignInClick)
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            RunActionButton(
+                text = stringResource(id = R.string.sign_up),
+                isLoading = false,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                onAction(IntroAction.OnSignUpClick)
+            }
+        }
+    }
+}
+
+@Composable
+private fun RunLogoVertical(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            imageVector = LogoIcon,
+            contentDescription = "Logo",
+            tint = MaterialTheme.colorScheme.onBackground
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = stringResource(id = R.string.app_name),
+            fontWeight = FontWeight.Medium,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun IntroScreenPreview() {
+    RunTheme {
+        IntroScreen {
+
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,6 +20,8 @@ dependencyResolutionManagement {
     }
 }
 
+gradle.startParameter.excludedTaskNames.addAll(listOf(":build-logic:convention:testClasses"))
+
 rootProject.name = "Runrun"
 // Access modules in type-safe manner
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
