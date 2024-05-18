baeba929c84ad242499c1ed9411c50fadfa108c5
DJDrama <putzzang@naver.com>
2024-05-19 00:42:50 +0900

    49. Scheduling syncs

--- run/data/src/main/java/com/dj/run/data/di/RunDataModule.kt
+++ run/data/src/main/java/com/dj/run/data/di/RunDataModule.kt
@@ -1,9 +1,13 @@
 package com.dj.run.data.di
 
+import com.dj.core.domain.run.SyncRunScheduler
 import com.dj.run.data.CreateRunWorker
 import com.dj.run.data.DeleteRunWorker
 import com.dj.run.data.FetchRunsWorker
+import com.dj.run.data.SyncRunWorkerScheduler
 import org.koin.androidx.workmanager.dsl.workerOf
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val runDataModule = module {
@@ -11,4 +15,5 @@ val runDataModule = module {
     workerOf(::FetchRunsWorker)
     workerOf(::DeleteRunWorker)
 
+    singleOf(::SyncRunWorkerScheduler).bind<SyncRunScheduler>()
 }
--- run/presentation/src/main/java/com/dj/run/presentation/run_overview/RunOverviewViewModel.kt
+++ run/presentation/src/main/java/com/dj/run/presentation/run_overview/RunOverviewViewModel.kt
@@ -6,18 +6,26 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dj.core.domain.run.RunRepository
+import com.dj.core.domain.run.SyncRunScheduler
 import com.dj.run.presentation.run_overview.mapper.toRunUi
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.minutes
 
 class RunOverviewViewModel(
-    private val runRepository: RunRepository
+    private val runRepository: RunRepository,
+    private val syncRunScheduler: SyncRunScheduler,
 ) : ViewModel() {
 
     var state by mutableStateOf(RunOverviewState())
 
     init {
+        viewModelScope.launch {
+            syncRunScheduler.scheduleSync(
+                type = SyncRunScheduler.SyncType.FetchRuns(interval = 30.minutes)
+            )
+        }
         runRepository.getRuns().onEach { runs ->
             val runUi = runs.map {
                 it.toRunUi()
