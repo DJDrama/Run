3d0b3e454714a1972308cdad0d3929e68d3572be
DJDrama <putzzang@naver.com>
2024-07-16 18:11:10 +0900

    57. [TEST] Share test utility in a multi-module project

---
+++ core/test/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    alias(libs.plugins.runrun.jvm.library)
+    alias(libs.plugins.runrun.jvm.junit5)
+}
+
+dependencies {
+    implementation(projects.core.domain)
+    //implementation(projects.core.connectivity.domain)
+    implementation(projects.run.domain)
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.junit5.api)
+    implementation(libs.coroutines.test)
+}
